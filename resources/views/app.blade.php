e6ccd51226e7d5e96a51472b9d5449493a9f41f7
matsjoonas <matsjoonas@gmail.com>
2022-05-17 23:07:29 +0300

    Add a section for developers

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>eID Easy</title>
+
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body class="font-inter antialiased bg-white text-gray-900 tracking-tight">
 <main class="flex-grow">
--- resources/views/developers.blade.php
+++ resources/views/developers.blade.php
@@ -1,11 +1,47 @@
-<h2 class="text-3xl text-center mb-8">
+<h2 class="text-4xl text-center mb-12 max-w-3xl ml-auto mr-auto">
     Wish to integrate document-signing into your own web-app/page or project?
 </h2>
 
-<div class="text-xl text-center">
-    We're developers. We know the pain, so we've made integrating with us is super easy!
+<div class="text-gray-800 text-center">
+    <p class="text-xl text-center">
+        We're developers. We know the pain, so we've made integrating with us super easy!
+        <br>
+        For example, this web-app makes literally only 2 API calls :)
+        <br>
+        But don't take our word on it - see for yourself at
+        <a
+                href="https://github.com/eideasy/simple-demo-php/blob/master/app/Http/Controllers/SignatureController.php"
+                target="_blank"
+                class="underline"
+        >
+            github!
+        </a>
+    </p>
+
+    <p class="text-xl text-center mt-6 mb-6">
+        You can also run this app on your own infrastructure via <span class="font-mono bg-teal-200">docker-compose up</span>
+        <br>or
+        <br>
+        one-click deploy to <a target="_blank" class="underline" href="https://cloud.google.com/run">Google Cloud Run</a>:
+
+    </p>
+
+    <div class="inline-block">
+        <a
+            href="https://deploy.cloud.run?git_repo=https://github.com/eideasy/simple-demo-php&revision=master"
+            rel="nofollow"
+        >
+            <img src="https://camo.githubusercontent.com/5832b6a44b19b13a5a84f8e89d646780a5ec646a0dfeef52f001134caca93511/68747470733a2f2f6465706c6f792e636c6f75642e72756e2f627574746f6e2e737667"
+                 alt="Run on Google Cloud"
+                 data-canonical-src="https://deploy.cloud.run/button.svg"
+                 style="max-width: 100%;">
+        </a>
+    </div>
+
+    <p class="text-xl text-center mt-12 mb-6">
+        PS! And don't forget to check out our extensive <a target="_blank"  class="underline" href="https://eideasy-docs.netlify.app/">documentation!</a>
+    </p>
 </div>
 
-<div class="text-xl text-center">
-    This web-app makes literally only 2 API calls
-</div>
+
+
